172254b5af1e73a511e45c193e7a9c9caac502b8
Paul Cernea <pcernea@gmail.com>
2024-08-21 14:21:49 -0700

    Indent.

--- computational_geometry/point_cloud.cpp
+++ computational_geometry/point_cloud.cpp
@@ -438,122 +438,122 @@ namespace ComputationalGeometry
 
   void PointCloud::unit_test()
   {
-  {
-    point_3d P(1.0, 2.0, 3.0);
-    std::cout << "\n//////\nThe dimension is " << P.GetDimension() << ".";
-    P.print("\n");
-    std::cout << "\n";
+    {
+      point_3d P(1.0, 2.0, 3.0);
+      std::cout << "\n//////\nThe dimension is " << P.GetDimension() << ".";
+      P.print("\n");
+      std::cout << "\n";
 
-    point_2d Q(1.0, 2.0);
-    std::cout << "\n//////\nThe dimension is " << Q.GetDimension() << ".";
-    Q.print("\n");
-    std::cout << "\n";
-  }
+      point_2d Q(1.0, 2.0);
+      std::cout << "\n//////\nThe dimension is " << Q.GetDimension() << ".";
+      Q.print("\n");
+      std::cout << "\n";
+    }
   
-  {
-    point_3d P(1.0, 2.0, 3.0);
-    point_3d Q(1.0, -2.0, 3.0);
-
-    printf("%f\n", point_3d::sq_distance(P,Q));
-  }
+    {
+      point_3d P(1.0, 2.0, 3.0);
+      point_3d Q(1.0, -2.0, 3.0);
   
-  {
-    point_2d P(1.5, 2.0);
-    point_2d Q(1.0, -2.0);
-
-    printf("%f\n", point_2d::sq_distance(P,Q));
-  }
+      printf("%f\n", point_3d::sq_distance(P,Q));
+    }
   
-  {
-    std::set<point_3d > A;
-    point_3d a(1.5, 2.0, 0);
-    point_3d b(1.0, 3.0, 0);
-    point_3d c(-1.5, 7.0, 0);
-    
-    std::set<point_3d > B;
-    point_3d d(4.5, 2.3, 0);
-    point_3d e(-1.55, 2.6, 0);
-    point_3d f(88.3, 0.001, 0);
-    
-    A.insert(a);
-    A.insert(b);
-    A.insert(c);
-    
-    B.insert(d);
-    B.insert(e);
-    B.insert(f);
-    
-    point_3d p, q;
-    
-    double min = point_3d::naive_min_sq_distance(A, B, p, q);
-    
-    std::cout << "\n//////\n" << min;
-    p.print("\n");
-    q.print("\n");
-    std::cout << "\n";
-  }
+    {
+      point_2d P(1.5, 2.0);
+      point_2d Q(1.0, -2.0);
   
-  {
-    std::set<point_2d > A;
-    point_2d a(1.5, 2.0);
-    point_2d b(1.0, 3.0);
-    point_2d c(-1.5, 7.0);
-    
-    std::set<point_2d > B;
-    point_2d d(4.5, 2.3);
-    point_2d e(-1.35, 2.6);
-    point_2d f(88.3, 0.001);
-    
-    A.insert(a);
-    A.insert(b);
-    A.insert(c);
-    
-    B.insert(d);
-    B.insert(e);
-    B.insert(f);
-    
-    point_2d p, q;
-    
-    double min = point_2d::naive_min_sq_distance(A, B, p, q);
-    
-    std::cout << "\n//////\n" << min << "\n";
-    p.print("\n");
-    q.print("\n");
-    std::cout << "\n";
-  }
+      printf("%f\n", point_2d::sq_distance(P,Q));
+    }
   
-  {
-    std::set<point_2d > A;
-    point_2d a(1.5, 2.0);
-    point_2d b(1.0, 3.0);
-    point_2d c(-1.5, 7.0);
-    point_2d d(4.5, 2.3);
-    point_2d e(-1.35, 2.6);
-    point_2d f(88.3, 0.001);
+    {
+      std::set<point_3d > A;
+      point_3d a(1.5, 2.0, 0);
+      point_3d b(1.0, 3.0, 0);
+      point_3d c(-1.5, 7.0, 0);
     
-    A.insert(a);
-    A.insert(b);
-    A.insert(c);
-    A.insert(d);
-    A.insert(e);
-    A.insert(f);
+      std::set<point_3d > B;
+      point_3d d(4.5, 2.3, 0);
+      point_3d e(-1.55, 2.6, 0);
+      point_3d f(88.3, 0.001, 0);
     
-    point_2d p, q;
+      A.insert(a);
+      A.insert(b);
+      A.insert(c);
     
-    double min = point_2d::naive_min_sq_distance(A, p, q);
+      B.insert(d);
+      B.insert(e);
+      B.insert(f);
     
-    std::cout << "\n//////\n";
-    std::cout << min << "\n";
-    p.print();  std::cout << "\n";
-    q.print();  std::cout << "\n";
+      point_3d p, q;
     
-    min = point_2d::min_sq_distance(A, p, q);
+      double min = point_3d::naive_min_sq_distance(A, B, p, q);
     
-    std::cout << "\n/!!!!/\n";
-    std::cout << min << "\n";
-    p.print();  std::cout << "\n";
-    q.print();  std::cout << "\n";
-  }
-  }
+      std::cout << "\n//////\n" << min;
+      p.print("\n");
+      q.print("\n");
+      std::cout << "\n";
+    }
+  
+    {
+      std::set<point_2d > A;
+      point_2d a(1.5, 2.0);
+      point_2d b(1.0, 3.0);
+      point_2d c(-1.5, 7.0);
+    
+      std::set<point_2d > B;
+      point_2d d(4.5, 2.3);
+      point_2d e(-1.35, 2.6);
+      point_2d f(88.3, 0.001);
+    
+      A.insert(a);
+      A.insert(b);
+      A.insert(c);
+    
+      B.insert(d);
+      B.insert(e);
+      B.insert(f);
+    
+      point_2d p, q;
+    
+      double min = point_2d::naive_min_sq_distance(A, B, p, q);
+    
+      std::cout << "\n//////\n" << min << "\n";
+      p.print("\n");
+      q.print("\n");
+      std::cout << "\n";
+    }
+  
+    {
+      std::set<point_2d > A;
+      point_2d a(1.5, 2.0);
+      point_2d b(1.0, 3.0);
+      point_2d c(-1.5, 7.0);
+      point_2d d(4.5, 2.3);
+      point_2d e(-1.35, 2.6);
+      point_2d f(88.3, 0.001);
+    
+      A.insert(a);
+      A.insert(b);
+      A.insert(c);
+      A.insert(d);
+      A.insert(e);
+      A.insert(f);
+    
+      point_2d p, q;
+    
+      double min = point_2d::naive_min_sq_distance(A, p, q);
+    
+      std::cout << "\n//////\n";
+      std::cout << min << "\n";
+      p.print();  std::cout << "\n";
+      q.print();  std::cout << "\n";
+    
+      min = point_2d::min_sq_distance(A, p, q);
+    
+      std::cout << "\n/!!!!/\n";
+      std::cout << min << "\n";
+      p.print();  std::cout << "\n";
+      q.print();  std::cout << "\n";
+    }
+  } // end of unit_test function.
 
 } // end of namespace ComputationalGeometry
