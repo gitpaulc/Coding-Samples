b80d7044cd1cf893baa53ef110a7080a21d882e2
Paul Cernea <pcernea@gmail.com>
2024-09-05 14:02:37 -0700

    Change build script

--- computational_geometry/compile_and_run_geometry.sh
+++ computational_geometry/compile_and_run_geometry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#open README.pdf
+#open README.md
 # Adding -w flag to suppress all warnings because of GLUT deprecation warnings for Mac OSX.
 # -std=c++20 is also possible.
 g++ -std=c++14 -o geometry_suite.o *.cpp -framework OpenGL -framework GLUT -w
