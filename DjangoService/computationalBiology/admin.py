cd93e9c216939e847acae2b0060d761afa89fccf
Paul Cernea <pcernea@gmail.com>
2024-09-09 15:08:19 -0700

    Improve structure of computational biology layout.

--- DjangoService/computationalBiology/admin.py
+++ DjangoService/computationalBiology/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
 
-from .models import Algorithm
+from django.contrib import admin
+from .models import InputString, Algorithm
 
 admin.site.register(Algorithm)
+admin.site.register(InputString)
--- DjangoService/computationalBiology/models.py
+++ DjangoService/computationalBiology/models.py
@@ -1,20 +1,20 @@
-from django.db import models
 
+from django.db import models
 
 class Algorithm(models.Model):
     whichAlgorithm = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         if self.whichAlgorithm == 0:
-            return 'Largest Common Subsequence'
+            return 'Longest Common Subsequence'
         #else:
-        return 'Largest Common Substring'
+            return 'Longest Common Substring'
 
 
-class InputModel(models.Model):
+class InputString(models.Model):
     algorithm = models.ForeignKey(Algorithm, on_delete=models.CASCADE)
     caption = models.CharField(max_length=200)
-    input1 = models.TextField(max_length=200)
-    input2 = models.TextField(max_length=200)
+    # inputField should contain default input.
+    inputField = models.TextField(max_length=1024)
     def __str__(self):
         return self.caption
