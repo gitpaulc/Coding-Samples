aabbbc5729ac7dd47d75245cedd74d65e02e6b19
Paul Cernea <pcernea@gmail.com>
2024-09-23 18:15:31 -0700

    Clean code.

--- computational_geometry/create_video.cpp
+++ computational_geometry/create_video.cpp
@@ -16,60 +16,86 @@ All Rights Reserved.*/
 #endif // not def VIDEO_OUTPUT_FOLDER
 
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 namespace ComputationalGeometry
 {
   bool createVideo(std::string& errMsg, VideoMode vm)
   {
     typedef cv::Point3_<uint8_t> Pixel;
-    cv::VideoCapture capture(0);
-    if (!capture.isOpened()) { errMsg = "Failed to open video stream.";  return false; }
 
-    const auto frameWidth = (int)capture.get(cv::CAP_PROP_FRAME_WIDTH);
-    const auto frameHeight = (int)capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+    int frameWidth = 0;
+    int frameHeight = 0;
 
     std::string videoOutFolder = VIDEO_OUTPUT_FOLDER;
-    cv::VideoWriter videoOut(videoOutFolder + "/compGeo.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frameWidth, frameHeight));
 
     uint8_t fewerColorsInterval = 30;
 
-    cv::Mat currentFrame;
     const char escapeKey = (char)27;
-    while (true)
+    std::vector<cv::Mat> arrayOfFrames;
     {
-      capture >> currentFrame;
-      if (currentFrame.empty()) { break; }
-      if ((vm == VideoMode::Grayscale) || (vm == VideoMode::FewerColors))
+      cv::VideoCapture capture(0);
+      if (!capture.isOpened()) { errMsg = "Failed to open video stream.";  return false; }
+      frameWidth = (int)capture.get(cv::CAP_PROP_FRAME_WIDTH);
+      frameHeight = (int)capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+      cv::Mat currentFrame;
+      cv::VideoWriter videoOut(videoOutFolder + "/compGeo.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frameWidth, frameHeight));
+      while (true)
       {
-        //auto prev = currentFrame;
-        //cv::cvtColor(prev, currentFrame, cv::COLOR_RGB2GRAY);
-        for (int r = 0; r < currentFrame.rows; ++r) {
-            Pixel* ptr = currentFrame.ptr<Pixel>(r, 0);
-            const Pixel* ptr_end = ptr + currentFrame.cols;
-            for (; ptr != ptr_end; ++ptr) {
-                if (vm == VideoMode::FewerColors)
-                {
-                  ptr->x = (ptr->x / fewerColorsInterval) * fewerColorsInterval;
-                  ptr->y = (ptr->y / fewerColorsInterval) * fewerColorsInterval;
-                  ptr->z = (ptr->z / fewerColorsInterval) * fewerColorsInterval;
-                  continue;
-                }
-                int avg = (ptr->x + ptr->y + ptr->z) / 3;
-                ptr->x = (uint8_t)avg;
-                ptr->y = (uint8_t)avg;
-                ptr->z = (uint8_t)avg;
+        capture >> currentFrame;
+        if (currentFrame.empty()) { break; }
+        if (vm == VideoMode::Usual)
+        {
+          arrayOfFrames.push_back(currentFrame.clone());
+        }
+        if ((vm == VideoMode::Grayscale) || (vm == VideoMode::FewerColors))
+        {
+          for (int row = 0; row < currentFrame.rows; ++row)
+          {
+            Pixel* pix = currentFrame.ptr<Pixel>(row, 0);
+            const Pixel* row_end = pix + currentFrame.cols;
+            for (; pix != row_end; ++pix)
+            {
+              if (vm == VideoMode::FewerColors)
+              {
+                pix->x = (pix->x / fewerColorsInterval) * fewerColorsInterval;
+                pix->y = (pix->y / fewerColorsInterval) * fewerColorsInterval;
+                pix->z = (pix->z / fewerColorsInterval) * fewerColorsInterval;
+                continue;
+              }
+              int avg = (pix->x + pix->y + pix->z) / 3;
+              pix->x = (uint8_t)avg;
+              pix->y = (uint8_t)avg;
+              pix->z = (uint8_t)avg;
             }
+          }
+        }
+        videoOut.write(currentFrame);
+        cv::imshow("Video - Press ESC to exit.", currentFrame); // Display current frame.
+        char keyboardKey = (char)cv::waitKey(1);
+        if (keyboardKey == escapeKey) { break; }
+      }
+      std::string imgOutFolder = IMG_OUTPUT_FOLDER;
+      cv::imwrite(imgOutFolder + "/compGeoScreenshot.png", currentFrame);
+    }
+    if (vm == VideoMode::Usual)
+    {
+      // Grayscale.
+      {
+        cv::VideoWriter videoOut(videoOutFolder + "/compGeoGrayscale.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frameWidth, frameHeight));
+        for (const auto& currentFrame : arrayOfFrames)
+        {
+          cv::Mat src = currentFrame.clone();
+          cv::Mat dst = currentFrame.clone();
+          cv::cvtColor(src, dst, cv::COLOR_RGB2GRAY);
+          cv::cvtColor(dst, src, cv::COLOR_GRAY2RGB);
+          videoOut.write(src);
         }
       }
-      videoOut.write(currentFrame);
-      cv::imshow("Video", currentFrame); // Display current frame.
-      char keyboardKey = (char)cv::waitKey(1);
-      if (keyboardKey == escapeKey) { break; }
     }
 
-    std::string imgOutFolder = IMG_OUTPUT_FOLDER;
-    cv::imwrite(imgOutFolder + "/compGeoScreenshot.png", currentFrame);
-
+    std::cout << "\nNum frames total = " << arrayOfFrames.size();
     return true;
   }
 }
