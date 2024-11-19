8ade84ab9260d7c67f81ececb7c44add1791c35e
Paul Cernea <pcernea@gmail.com>
2024-11-19 11:46:16 -0800

    Camera

---
+++ mesh_renderer/camera.h
@@ -0,0 +1,13 @@
+/*  Copyright Paul Cernea, August 2024.
+All Rights Reserved.*/
+
+#ifndef GL_CALLBACKS_H
+#define GL_CALLBACKS_H
+
+static int& GetWindowId();
+void initialize_glut(int* argc_ptr, char** argv);
+void keyboard(unsigned char key, int x, int y);
+void mouse(int button, int state, int x, int y);
+void render();
+
+#endif //def GL_CALLBACKS_H
