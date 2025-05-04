0f97db28b5d420fba2dd8010cb7f3c1f492092b1
gitpaulc <pcernea@gmail.com>
2025-05-03 21:43:22 -0700

    Added functionality to pan screen.
    Added functionality to pan screen, as well as sine, hyperbola, e^x, ln(x), and sinc special function, as well as sample screenshots.

--- graphing_math/main.cpp
+++ graphing_math/main.cpp
@@ -7,10 +7,17 @@ int main(int argc, char **argv)
   std::cout << "\n\tGraph a 2d function f(x, y) = 0 by default.";
   std::cout << "\n\tPress \"M\" to view the positive/negative sets of a function z = f(x, y). Red > 0, Blue < 0.";
   std::cout << "\n\tPress \"Z\" to zoom in, \"Y\" to zoom out, \"R\" to rotate clockwise, \"T\" to rotate counter-clockwise.";
+  std::cout << "\n\tPress \"I\" to scroll up, \"K\" to scroll down, \"J\" to scroll left, \"L\" to scroll right.";
+  std::cout << "\n\tPress \"Q\" to or ESC to exit.";
   std::cout << "\n\tGraph a line by default or enter the number 0 or the word \"line\" to graph a line. Similarly...";
   std::cout << "\n\t1. parabola";
   std::cout << "\n\t2. cubic";
   std::cout << "\n\t3. circle";
+  std::cout << "\n\t4. hyperbola";
+  std::cout << "\n\t5. sine";
+  std::cout << "\n\t6. sinc";
+  std::cout << "\n\t7. exp";
+  std::cout << "\n\t8. log";
   std::cout << std::endl;
   std::string graphType = "";
   if (argc >= 2)
--- graphing_math/math.cpp
+++ graphing_math/math.cpp
@@ -1,10 +1,14 @@
 #include "math.h"
+#include <stdexcept>
+#include <cmath>
 
 namespace // anonymous
 {
   void rescale(double& x, double& y)
   {
     Math& math = Math::Get();
+    x = x - math.origin_x;
+    y = y - math.origin_y;
     double cosTheta = cos(math.angle);
     double sinTheta = sin(math.angle);
     double xTemp = x * cosTheta + y * (-sinTheta);
@@ -38,6 +42,71 @@ namespace // anonymous
     rescale(x, y);
     return 1.0 - x * x - y * y;
   }
+
+  double hyperbola(double x, double y)
+  {
+    rescale(x, y);
+    if (abs(x) <= 1.0e-9)
+    {
+      throw std::runtime_error("Division by zero.");
+      return 1;
+    }
+    double ans = 1.0 / x;
+    return y - ans;
+  }
+
+  double sine(double x, double y)
+  {
+    rescale(x, y);
+    double ans = sin(x);
+    return y - ans;
+  }
+
+  double sinc(double x, double y)
+  {
+    rescale(x, y);
+    if (abs(x) <= 1.0e-6)
+    {
+      double x2 = x * x;
+      double ans = 1.0 - x2 / 6.0 + x2 * x2 / 120.0;
+      return y - ans;
+    }
+    double ans = sin(x) / x;
+    return y - ans;
+  }
+
+  double expOneVar(double x)
+  {
+    if (abs(x) >= 1.0)
+    {
+      double ans = expOneVar(x / 2.0);
+      return ans * ans;
+    }
+    double xPower = 1.0;
+    double ans = 1.0;
+    double factorialNum = 1.0;
+    for (int i = 1; i <= 10; ++i)
+    {
+      xPower *= x;
+      factorialNum *= i;
+      ans += xPower / factorialNum;
+    }
+    return ans;
+  }
+
+  double exponential(double x, double y)
+  {
+    rescale(x, y);
+    //return y - expOneVar(x);
+    return y - std::exp(x);
+  }
+
+  double natlog(double x, double y)
+  {
+    rescale(x, y);
+    //return expOneVar(y) - x;
+    return y - std::log(x);
+  }
 }
 
 Math::Math()
@@ -70,7 +139,38 @@ void Math::SetType(const std::string& mathType)
   }
   if ((mathType.compare("3") == 0) || (mathType.compare("circle") == 0))
   {
+    scale = 1.5;
     Function = &circle;
+    return;
+  }
+  if ((mathType.compare("4") == 0) || (mathType.compare("hyperbola") == 0))
+  {
+    scale = 4;
+    Function = &hyperbola;
+    return;
+  }
+  if ((mathType.compare("5") == 0) || (mathType.compare("sine") == 0))
+  {
+    scale = 6;
+    Function = &sine;
+    return;
+  }
+  if ((mathType.compare("6") == 0) || (mathType.compare("sinc") == 0))
+  {
+    scale = 10;
+    Function = &sinc;
+    return;
+  }
+  if ((mathType.compare("7") == 0) || (mathType.compare("exp") == 0))
+  {
+    scale = 4;
+    Function = &exponential;
+    return;
+  }
+  if ((mathType.compare("8") == 0) || (mathType.compare("log") == 0))
+  {
+    scale = 4;
+    Function = &natlog;
     return;
   }
   Function = &linear;
