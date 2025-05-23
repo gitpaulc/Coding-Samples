128beba9dac546ab132865b97311edcb7ea8fc3a
gitpaulc <pcernea@gmail.com>
2025-05-23 16:50:50 -0700

    Factor out special cases.
    Factor out special cases.

--- graphing_math/math.cpp
+++ graphing_math/math.cpp
@@ -167,13 +167,10 @@ namespace // anonymous
     double xx = x;
     double yy = y;
     double zz = 0.0;
-    double coeff = kk;
     double phase = piNum / 2.0;
-    if (mm == 3)
-    {
-      if (!math.hasParam2) { kk = 8.0 * piNum; }
-      phase = kk / 6.0;
-    }
+    if (!math.hasParam2) { kk = mm - 2.0; }
+    kk *= piNum;
+    phase = kk / mm;
 
     double theta = 2.0 * piNum / mm0;
     double cosTheta = cos(theta);
@@ -182,7 +179,7 @@ namespace // anonymous
     double sinAngle = 0.0;
     for (int ii = 0; ii < mm; ++ii)
     {
-      double summand = sin(coeff * (cosAngle * xx - sinAngle * yy) + phase);
+      double summand = sin((cosAngle * xx - sinAngle * yy) + phase);
       zz += summand;
       double tempCos = cosAngle;
       double tempSin = sinAngle;
