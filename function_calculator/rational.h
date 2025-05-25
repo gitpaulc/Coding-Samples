a2d111ec2afb51f0cdb15ecc2c2f420854540803
Paul Cernea <pcernea@gmail.com>
2025-05-24 18:18:31 -0700

    Create Function Calculator project.

---
+++ function_calculator/rational.h
@@ -0,0 +1,40 @@
+/*  Copyright Paul Cernea, August 2024.
+All Rights Reserved.*/
+
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+namespace FunctionalCalculator
+{
+
+class Rational
+{
+  int num = 0;
+  int denom = 1;
+public:
+  Rational(int nn = 0, int dd = 1);
+
+  Rational(const Rational&);
+  Rational(Rational&&) noexcept;
+  Rational& operator=(const Rational&);
+  Rational& operator=(Rational&&) noexcept;
+  ~Rational() = default;
+    
+  static int gcd(int aa, int bb);
+  Rational operator+() const;
+  Rational operator-() const;
+  Rational operator+(const Rational& rhs) const;
+  Rational operator-(const Rational& rhs) const;
+  Rational operator*(const Rational& rhs) const;
+  Rational operator/(const Rational& rhs) const;
+  bool operator==(const Rational& rhs) const;
+  bool operator!=(const Rational& rhs) const;
+  bool operator<(const Rational& rhs) const;
+  bool operator>(const Rational& rhs) const;
+  bool operator<=(const Rational& rhs) const;
+  bool operator>=(const Rational& rhs) const;
+  double get() const;
+};
+}
+
+#endif //def RATIONAL
