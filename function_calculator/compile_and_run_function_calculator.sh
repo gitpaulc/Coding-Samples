98a9364c10bd8277c236d6afce0cd31465c5b86b
Paul Cernea <pcernea@gmail.com>
2025-05-25 19:31:26 -0700

    Ensure build on Mac succeeds.

--- function_calculator/compile_and_run_function_calculator.sh
+++ function_calculator/compile_and_run_function_calculator.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-main()
-{
-  local SCRIPT_DIR="$(pwd)"
-  cd "${SCRIPT_DIR}"
-
-  g++ *.cpp -o functional_calculator.o
-
-  if [[ -a functional_calculator.o ]] ; then
-    ./functional_calculator.o "${1}" "${2}" "${3}"
-  fi
-}
-
-time main "${1}" "${2}" "${3}"
+g++ -std=c++14 -o function_calculator.o *.cpp -framework OpenGL -framework GLUT -w
+./function_calculator.o "${1}"
