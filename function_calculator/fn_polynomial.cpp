0c238b8daa50f3be62e363a887ebd64c0f7c5318
Paul Cernea <pcernea@gmail.com>
2025-06-08 16:12:13 -0700

    Y and Z evaluation.

--- function_calculator/fn_polynomial.cpp
+++ function_calculator/fn_polynomial.cpp
@@ -935,4 +935,101 @@ namespace FunctionalCalculator
     return true;
   }
 
+  bool FnPolynomial::tryEvaluateAtY(const ComplexQuadratic& yVal, FnPolynomial& output) const
+  {
+    FnPolynomial answer;
+    for (const auto& iter : self)
+    {
+      auto newKey = iter.first;
+      auto newVal = iter.second;
+      if (newKey.yInd != 0)
+      {
+        newVal = newVal * PiPolynomial(yVal.pow(newKey.yInd));
+        newKey.yInd = 0;
+      }
+      if (newKey.ePiYInd != 0)
+      {
+        if (yVal != 0) { return false; }
+        newKey.ePiYInd = QuadraticNumber();
+      }
+      if (newKey.trigPiYInd != TrigIndex())
+      {
+        ComplexQuadratic trigInput = yVal * newKey.trigPiYInd.self;
+        if (trigInput.getIm() != 0) { return false; }
+        Rational rationalVal;
+        bool isRational = trigInput.getRe().getRational(rationalVal);
+        if (!isRational) { return false; }
+        if (newKey.trigPiYInd.isCos())
+        {
+          QuadraticNumber factor;
+          bool trigSuccess = QuadraticNumber::tryGetCosine(rationalVal, factor);
+          if (!trigSuccess) { return false; }
+          newVal = newVal * PiPolynomial(ComplexQuadratic(factor));
+        }
+        else
+        {
+          QuadraticNumber factor;
+          bool trigSuccess = QuadraticNumber::tryGetSine(rationalVal, factor);
+          if (!trigSuccess) { return false; }
+          newVal = newVal * PiPolynomial(ComplexQuadratic(factor));
+        }
+        newKey.trigPiYInd = TrigIndex();
+      }
+      auto newIter = answer.self.find(newKey);
+      if (newIter == answer.self.end()) { answer.self[newKey] = newVal; }
+      else { newIter->second = newIter->second + newVal; }
+    }
+    answer.clean();
+    output = answer;
+    return true;
+  }
+
+  bool FnPolynomial::tryEvaluateAtZ(const ComplexQuadratic& zVal, FnPolynomial& output) const
+  {
+    FnPolynomial answer;
+    for (const auto& iter : self)
+    {
+      auto newKey = iter.first;
+      auto newVal = iter.second;
+      if (newKey.zInd != 0)
+      {
+        newVal = newVal * PiPolynomial(zVal.pow(newKey.zInd));
+        newKey.zInd = 0;
+      }
+      if (newKey.ePiZInd != 0)
+      {
+        if (zVal != 0) { return false; }
+        newKey.ePiZInd = QuadraticNumber();
+      }
+      if (newKey.trigPiZInd != TrigIndex())
+      {
+        ComplexQuadratic trigInput = zVal * newKey.trigPiZInd.self;
+        if (trigInput.getIm() != 0) { return false; }
+        Rational rationalVal;
+        bool isRational = trigInput.getRe().getRational(rationalVal);
+        if (!isRational) { return false; }
+        if (newKey.trigPiZInd.isCos())
+        {
+          QuadraticNumber factor;
+          bool trigSuccess = QuadraticNumber::tryGetCosine(rationalVal, factor);
+          if (!trigSuccess) { return false; }
+          newVal = newVal * PiPolynomial(ComplexQuadratic(factor));
+        }
+        else
+        {
+          QuadraticNumber factor;
+          bool trigSuccess = QuadraticNumber::tryGetSine(rationalVal, factor);
+          if (!trigSuccess) { return false; }
+          newVal = newVal * PiPolynomial(ComplexQuadratic(factor));
+        }
+        newKey.trigPiZInd = TrigIndex();
+      }
+      auto newIter = answer.self.find(newKey);
+      if (newIter == answer.self.end()) { answer.self[newKey] = newVal; }
+      else { newIter->second = newIter->second + newVal; }
+    }
+    answer.clean();
+    output = answer;
+    return true;
+  }
 }
--- function_calculator/fn_polynomial.h
+++ function_calculator/fn_polynomial.h
@@ -147,6 +147,22 @@ public:
    */
   bool tryEvaluateAtX(const ComplexQuadratic& xVal, FnPolynomial& output) const;
 
+  /** \return `true` if and only if evaluation succeeds. Only then is the `output` parameter written.
+   *  \remark Currently when a is a complex number and (a)^p is attempted for nonnegative integers p, it should always succeed.
+   *  \remark Currently when a is a real number and e^a is attempted, evaluation only succeeds for a == 0. This is because e polynomials are unsupported.
+   *  \remark Currently when a is a real QuadraticNumber and cos(pi * a) or sin(pi * a) are attempted, they should only succeed for well-known trig values.
+   *  This means that a should be a multiple of 1/12 so that pi * a includes the usual values of pi / 2, pi / 4, pi / 3, and pi / 6.
+   */
+  bool tryEvaluateAtY(const ComplexQuadratic& xVal, FnPolynomial& output) const;
+
+  /** \return `true` if and only if evaluation succeeds. Only then is the `output` parameter written.
+   *  \remark Currently when a is a complex number and (a)^p is attempted for nonnegative integers p, it should always succeed.
+   *  \remark Currently when a is a real number and e^a is attempted, evaluation only succeeds for a == 0. This is because e polynomials are unsupported.
+   *  \remark Currently when a is a real QuadraticNumber and cos(pi * a) or sin(pi * a) are attempted, they should only succeed for well-known trig values.
+   *  This means that a should be a multiple of 1/12 so that pi * a includes the usual values of pi / 2, pi / 4, pi / 3, and pi / 6.
+   */
+  bool tryEvaluateAtZ(const ComplexQuadratic& zVal, FnPolynomial& output) const;
+
   friend class Function;
 };
 }
