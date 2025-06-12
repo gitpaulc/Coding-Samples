e934a88e78595792d9c75bd5e0146ea83175aa7d
Paul Cernea <pcernea@gmail.com>
2025-06-12 16:49:41 -0700

    Advanced test.

--- function_calculator/main.cpp
+++ function_calculator/main.cpp
@@ -542,6 +542,10 @@ bool test_quadratic()
   reciprocal = QuadraticNumber(1) / sumOfSquareRoots;
   std::cout << "\nThe reciprocal of " << sumOfSquareRoots.print() << " is:\n" << reciprocal.print();
   std::cout << "\nOne = " << (reciprocal * sumOfSquareRoots).print();
+  sumOfSquareRoots = QuadraticNumber::sqrt(7) + QuadraticNumber::sqrt(5) + QuadraticNumber::sqrt(3) + Rational(1);
+  reciprocal = QuadraticNumber(1) / sumOfSquareRoots;
+  std::cout << "\nThe reciprocal of " << sumOfSquareRoots.print() << " is:\n" << reciprocal.print();
+  std::cout << "\nOne = " << (reciprocal * sumOfSquareRoots).print();
   return true;
 }
 
