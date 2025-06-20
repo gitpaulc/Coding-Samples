49fc01fd9a6850f8e1d49acd0a0d24d1b2848d66
Paul Cernea <pcernea@gmail.com>
2025-06-20 14:55:39 -0700

    Add UI controls.

--- function_calculator_cs/MainWindow.Designer.cs
+++ function_calculator_cs/MainWindow.Designer.cs
@@ -6,6 +6,9 @@ namespace function_calculator_cs
 {
   partial class MainWindow
   {
+    private Button continueBtn;
+    private Button endCurrentTest;
+    private TextBox console;
     private IContainer components = null;
 
     protected override void Dispose(bool disposing)
@@ -18,12 +21,45 @@ namespace function_calculator_cs
 
     private void InitializeComponent()
     {
-      components = new Container();
+      continueBtn = new Button();
+      endCurrentTest = new Button();
+      console = new TextBox();
+      SuspendLayout();
+
+      continueBtn.Location = new Point(694, 456);
+      continueBtn.Name = "continueBtn";
+      continueBtn.Size = new Size(94, 29);
+      continueBtn.TabIndex = 0;
+      continueBtn.Text = "Continue";
+      continueBtn.UseVisualStyleBackColor = true;
+
+      endCurrentTest.Location = new Point(550, 456);
+      endCurrentTest.Name = "endCurrentTest";
+      endCurrentTest.Size = new Size(138, 29);
+      endCurrentTest.TabIndex = 1;
+      endCurrentTest.Text = "End Current Test";
+      endCurrentTest.UseVisualStyleBackColor = true;
+
+      console.Location = new Point(12, 12);
+      console.Multiline = true;
+      console.Name = "console";
+      console.Size = new Size(776, 438);
+      console.ReadOnly = true;
+      console.TabIndex = 2;
+
+      AutoScaleDimensions = new SizeF(8F, 20F);
       AutoScaleMode = AutoScaleMode.Font;
       ClientSize = new Size(800, 600);
+      Controls.Add(console);
+      Controls.Add(endCurrentTest);
+      Controls.Add(continueBtn);
+      Name = "MainWindow";
       Text = "Function Calculator";
+
+      ResumeLayout(false);
+      PerformLayout();
     }
 
-    #endregion
+    #endregion // UI Code
   }
 }
