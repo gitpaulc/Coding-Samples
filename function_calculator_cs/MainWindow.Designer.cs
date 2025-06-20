d24b5ae6eb8b2c80e4c45adb053e896391c4b07e
Paul Cernea <pcernea@gmail.com>
2025-06-20 14:35:58 -0700

    Clean code.

--- function_calculator_cs/MainWindow.Designer.cs
+++ function_calculator_cs/MainWindow.Designer.cs
@@ -1,15 +1,16 @@
-﻿namespace function_calculator_cs
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace function_calculator_cs
 {
   partial class MainWindow
   {
-    private System.ComponentModel.IContainer components = null;
+    private IContainer components = null;
 
     protected override void Dispose(bool disposing)
     {
-      if (disposing && (components != null))
-      {
-        components.Dispose();
-      }
+      if (disposing && (components != null)) { components.Dispose(); }
       base.Dispose(disposing);
     }
 
@@ -17,10 +18,10 @@
 
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 600);
-      this.Text = "Function Calculator";
+      components = new Container();
+      AutoScaleMode = AutoScaleMode.Font;
+      ClientSize = new Size(800, 600);
+      Text = "Function Calculator";
     }
 
     #endregion
