af351fb7fa37f288b1b42c3caee9a91c1ab49ece
Paul Cernea <pcernea@gmail.com>
2025-07-01 11:02:25 -0700

    Make sqrt visible.

--- function_calculator_cs/MainWindow.cs
+++ function_calculator_cs/MainWindow.cs
@@ -311,7 +311,7 @@ namespace function_calculator_cs
     {
       var unity = new QuadraticNumber(new Rational(1));
       console.Text = "";
-      for (int lim_ = 8; lim_ < 11; ++lim_)
+      for (int lim_ = 8; lim_ < 9; ++lim_)
       {
         var sumOfSquareRoots = new QuadraticNumber();
         for (int ii = 0; ii < lim_; ++ii)
@@ -332,7 +332,6 @@ namespace function_calculator_cs
     private void endTests()
     {
       console.Text = "";
-      checkSqrt.Visible = false;
       testState = new TestingState();
       testBtn.Visible = true;
       continueBtn.Visible = false;
