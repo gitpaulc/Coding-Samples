aac78b36245b1c9543cd045f87226474e7360b56
Paul Cernea <pcernea@gmail.com>
2025-07-03 18:43:28 -0700

    Add test.

--- function_calculator/main.cpp
+++ function_calculator/main.cpp
@@ -31,10 +31,10 @@ bool test_evaluation()
     success = fn.tryEvaluateAtX(oneFourth, result);
     if (!success) { return false; }
     std::cout << "\n\ncos(pi / 4) = " << result.print();
-    success = fn.tryEvaluateAtX(oneSixth, result);
+    success = fn.tryEvaluateAtX(oneFifth, result);
     if (!success) { return false; }
     std::cout << "\n\ncos(pi / 5) = " << result.print();
-    success = fn.tryEvaluateAtX(oneFifth, result);
+    success = fn.tryEvaluateAtX(oneSixth, result);
     if (!success) { return false; }
     std::cout << "\n\ncos(pi / 6) = " << result.print();
     success = fn.tryEvaluateAtX(oneTwelfth, result);
@@ -75,10 +75,10 @@ bool test_evaluation()
     success = fn.tryEvaluateAtX(oneFourth, result);
     if (!success) { return false; }
     std::cout << "\n\nsin(pi / 4) = " << result.print();
-    success = fn.tryEvaluateAtX(oneSixth, result);
+    success = fn.tryEvaluateAtX(oneFifth, result);
     if (!success) { return false; }
     std::cout << "\n\nsin(pi / 5) = " << result.print();
-    success = fn.tryEvaluateAtX(oneFifth, result);
+    success = fn.tryEvaluateAtX(oneSixth, result);
     if (!success) { return false; }
     std::cout << "\n\nsin(pi / 6) = " << result.print();
     success = fn.tryEvaluateAtX(oneTwelfth, result);
